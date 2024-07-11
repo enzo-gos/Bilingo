1406b938a9e9fe8a53146c8ae023351efadd78df
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-07-11 14:58:52 +0700

    refactoring

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,32 +1,23 @@
 module Admin
   class ReportsController < BaseController
+    before_action :prepare_report, except: [:index]
+
     def index
       @story_reports = StoryReport.includes([:reporter, { story: [:author, { cover_image_attachment: :blob }] }]).all.order(status: :asc, created_at: :desc)
     end
 
     def show
-      @report = StoryReport.find(params[:id])
-      authorize @report, policy_class: Admin::StoryReportPolicy
-
       @requests = @report.banned_requests
       @story = @report.story
     end
 
     def close
-      @report_request = StoryReport.find(params[:id])
-      authorize @report_request, policy_class: Admin::StoryReportPolicy
-
-      @report_request.closed!
-
+      @report.closed!
       redirect_back fallback_location: root_path, notice: 'Reported request was successfully closed.'
     end
 
     def accept
-      @report_request = StoryReport.find(params[:id])
-      authorize @report_request, policy_class: Admin::StoryReportPolicy
-
-      @report_request.accepted!
-
+      @report.accepted!
       redirect_back fallback_location: root_path, notice: 'Reported request was successfully closed.'
     end
 
@@ -34,6 +25,11 @@ module Admin
 
     def report_request_params
       params.require(:report_request).permit(:title, :reason)
+    end
+
+    def prepare_report
+      @report = StoryReport.find(params[:id])
+      authorize @report, policy_class: Admin::StoryReportPolicy
     end
   end
 end
