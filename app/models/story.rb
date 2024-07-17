8bfa9b69906e23ebf0c4b264df5711ccb7a02cf2
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-07-17 17:04:19 +0700

    fix top views

--- app/models/story.rb
+++ app/models/story.rb
@@ -21,7 +21,7 @@ class Story < ApplicationRecord
             :language_code,
             presence: true
 
-  scope :with_published, -> { joins(:chapters).where(chapters: { published: true }, banned: false) }
+  scope :with_published, -> { joins(:chapters).where(chapters: { published: true }, banned: false).distinct }
 
   scope :top_viewed, ->(limit = 3) {
     subquery = StoryView.select('story_id, COUNT(*) AS view_count').group(:story_id)
@@ -30,7 +30,7 @@ class Story < ApplicationRecord
       .select('stories.*, COALESCE(subquery.view_count, 0) AS view_count')
       .joins("LEFT OUTER JOIN (#{subquery.to_sql}) AS subquery ON subquery.story_id = stories.id")
       .group('stories.id, subquery.view_count')
-      .order('subquery.view_count DESC NULLS LAST, stories.id')
+      .order(Arel.sql('COALESCE(subquery.view_count, 0) DESC, stories.id'))
       .limit(limit)
   }
 
