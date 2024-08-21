c06eab3449dab596ea5cfdbfcfed4c61d065bcfc
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-08-21 22:02:36 +0700

    cralw story

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
   def show
     @author = AuthorInfor.find(params[:id])
-    @stories = @author.stories.includes(:primary_genre, :secondary_genre, :author, { cover_image_attachment: :blob })
+    @stories = @author.stories.includes(:primary_genre, :secondary_genre, :author, :rich_text_description, { cover_image_attachment: :blob })
 
     @published = @stories.where(id: Chapter.where(published: true).select(:story_id)).size
     @draft = @stories.size - @published
